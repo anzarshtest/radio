c420ea7b13b7a83eed31d7fb9c4513dac357bf4d
anzarshtest <anzarshtest@mail.ru>
2017-02-06 14:00:50 +0300

    player

--- js/common.js
+++ js/common.js
@@ -1,5 +1,20 @@
+
 $(document).ready(function(){
     // $('.load').animate({'opacity': '0', 'z-index': '0'}, 1000);
+    Player = {
+            init  : function(audio, pp, time, volicon, vbar, stname){this.audio = audio;this.ppcontrol = pp;this.volicon = volicon;this.timebox = time;this.vbar = vbar;this.stname = stname;this.updVol();},
+            vol   : function(i){this.audio.volume = i/100;this.updVol();},
+            updVol: function(){i = this.audio.volume;this.volicon.className = this.audio.muted ? 'muted' : (i > 0.3 ? (i > 0.6 ? 'high' : 'medium') : 'low');},
+            track : function(e){t = e.getAttribute('data-uri');n = e.getAttribute('data-title');this.audio.src = t;this.stname.innerHTML = n;this.play();},
+            play  : function(){if(this.audio.src == ''){this.stname.innerHTML = 'Пожалуйста, выберите станцию!';} else {this.audio.play();this.ppcontrol.className = 'pause';this.time();}},
+            pause : function(){this.audio.pause();this.ppcontrol.className = 'play';clearTimeout(this.timer);},
+            time  : function(){t = this.audio.currentTime;h = parseInt(t/3600); hh = h > 0 ? h+' : ' : '';m = parseInt((t-h*3600)/60); mm = m > 9 ? m :'0'+m;s = parseInt(t-h*3600-m*60); ss = s > 9 ? s : '0'+s;this.timebox.innerHTML = hh+mm+' : '+ss;this.timer = setTimeout(function(){Player.time();}, 500);},
+            pp    : function(){if(this.audio.paused){this.play();} else {this.pause();}},
+            mute  : function(){this.audio.muted = !this.audio.muted;this.updVol();}
+        };function $1(e){return document.getElementById(e);}
+        Player.init($1('audio'), $1('pp'), $1('time'), $1('volicon'), $1('vbar'), $1('stationname'));
+
+  
     $('.radious a').click(function(){
         $('.radious a').removeClass('active');
         $(this).addClass('active');
